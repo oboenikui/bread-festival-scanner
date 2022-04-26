94b45806179ed7a5f8fa7129beb40823b04e65b5
oboenikui <oboenikuitwitter@gmail.com>
2022-04-27 04:22:57 +0900

    fix camera whiteout bug on Pixle 5

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -7,11 +7,6 @@ import { AppButton } from "./AppButton";
 
 const DEFAULT_VIDEO_WIDTH = 480;
 const DEFAULT_VIDEO_HEIGHT = 640;
-const DEFAULT_VIDEO_CONSTRAINS: MediaStreamConstraints["video"] = {
-    aspectRatio: 3 / 4,
-    width: DEFAULT_VIDEO_WIDTH,
-    height: DEFAULT_VIDEO_HEIGHT,
-}
 
 const CONSECUTIVE_TIMES_THRESHOLD = 3;
 
@@ -28,10 +23,21 @@ const useAnimationFrame = (callback = () => { }) => {
     }, [loop]);
 };
 
+
+function videoConstrains(): MediaStreamConstraints["video"] {
+    const isPortrait = window.screen.orientation.type.includes("portrait")
+    return {
+        aspectRatio: isPortrait ? 4 / 3 : 3 / 4,
+        width: isPortrait ? DEFAULT_VIDEO_HEIGHT : DEFAULT_VIDEO_WIDTH,
+        height: isPortrait ? DEFAULT_VIDEO_WIDTH : DEFAULT_VIDEO_HEIGHT,
+        facingMode: "environment",
+    };
+}
+
 export const Scanner: React.FC = () => {
     const webcamRef = useRef<Webcam>(null);
     const inputCanvasRef = useRef<HTMLCanvasElement>(null);
-    const [videoConstraints, setVideoConstrains] = useState<MediaStreamConstraints["video"]>(DEFAULT_VIDEO_CONSTRAINS);
+    const [videoConstraints, setVideoConstrains] = useState<MediaStreamConstraints["video"]>(videoConstrains());
     const [sum, setSum] = useState(0);
     const [consecutiveTimes, setConsecutiveTimes] = useState(0);
     const [showResult, setShowResult] = useState(false);
@@ -40,26 +46,9 @@ export const Scanner: React.FC = () => {
     const [resultBadgeStyles, setResultBadgeStyles] = useState<CSSProperties[]>([]);
 
     useEffect(() => {
-        navigator.mediaDevices.enumerateDevices()
-            .then(devices => {
-                if (devices.filter(device => device.kind === "videoinput").length > 1) {
-                    setVideoConstrains(v => Object.assign({}, v, {
-                        facingMode: { exact: "environment" },
-                    }));
-                }
-            })
-
-    }, []);
-
-    useEffect(() => {
 
         const configure = () => {
-            const isPortrait = window.screen.orientation.type.includes("portrait")
-            setVideoConstrains(v => Object.assign({}, v, {
-                aspectRatio: 3 / 4,
-                width: isPortrait ? DEFAULT_VIDEO_HEIGHT : DEFAULT_VIDEO_WIDTH,
-                height: isPortrait ? DEFAULT_VIDEO_WIDTH : DEFAULT_VIDEO_HEIGHT,
-            }));
+            setVideoConstrains(videoConstrains());
         }
         configure();
         window.screen.orientation.addEventListener("change", configure);
@@ -88,7 +77,6 @@ export const Scanner: React.FC = () => {
         const context = canvasElement.getContext("2d");
         context?.drawImage(imageSrc, 0, 0);
 
-        // try {
         const img = cv.imread(canvasElement);
         const result = detectStickers(img);
 
@@ -97,36 +85,29 @@ export const Scanner: React.FC = () => {
             setConsecutiveTimes(pre => pre + 1);
 
             if (consecutiveTimes + 1 >= CONSECUTIVE_TIMES_THRESHOLD) {
-                setShowResult(() => true);
-                setResult(() => result);
+                setShowResult(true);
+                setResult(result);
 
                 const imageWidth = canvasElement.width;
                 const imageHeight = canvasElement.height;
-                setResultShadowStyles(() =>
-                    result.map(sticker => ({
-                        left: `${(sticker.circle.x - sticker.circle.radius) / imageWidth * 100}%`,
-                        top: `${(sticker.circle.y - sticker.circle.radius) / imageHeight * 100}%`,
-                        width: `${(sticker.circle.radius * 2) / imageWidth * 100}%`,
-                        height: `${(sticker.circle.radius * 2) / imageHeight * 100}%`,
-                    })));
+                setResultShadowStyles(result.map(sticker => ({
+                    left: `${(sticker.circle.x - sticker.circle.radius) / imageWidth * 100}%`,
+                    top: `${(sticker.circle.y - sticker.circle.radius) / imageHeight * 100}%`,
+                    width: `${(sticker.circle.radius * 2) / imageWidth * 100}%`,
+                    height: `${(sticker.circle.radius * 2) / imageHeight * 100}%`,
+                })));
 
-                setResultBadgeStyles(() =>
-                    result.map(sticker => ({
-                        left: `${sticker.circle.x / imageWidth * 100}%`,
-                        top: `${(sticker.circle.y - sticker.circle.radius) / imageHeight * 100}%`,
-                    })));
+                setResultBadgeStyles(result.map(sticker => ({
+                    left: `${sticker.circle.x / imageWidth * 100}%`,
+                    top: `${(sticker.circle.y - sticker.circle.radius) / imageHeight * 100}%`,
+                })));
             }
         } else {
-            setConsecutiveTimes(() => 0);
+            setConsecutiveTimes(0);
         }
-        setSum(() => nextSum);
-
+        setSum(nextSum);
 
         img.delete();
-
-        // } catch (error) {
-        //     console.log(error);
-        // }
     }
     useAnimationFrame(detect);
 
